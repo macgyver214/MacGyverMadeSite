cd6cdd86cd6b91e8a34c89b4ac3e9714053fa4bf
Leif Swenson <lfswenson@gmail.com>
2020-03-01 01:18:31 -0700

    Added keyboards component

---
+++ src/app/keyboards/keyboards.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KeyboardsComponent } from './keyboards.component';
+
+describe('KeyboardsComponent', () => {
+  let component: KeyboardsComponent;
+  let fixture: ComponentFixture<KeyboardsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ KeyboardsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KeyboardsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/keyboards/keyboards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-keyboards',
+  templateUrl: './keyboards.component.html',
+  styleUrls: ['./keyboards.component.less']
+})
+export class KeyboardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
