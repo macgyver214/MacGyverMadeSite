a39de4e530a845e4ae06f238407b8b247551f580
Leif Swenson <lfswenson@gmail.com>
2020-03-04 22:08:16 -0700

    added a blog component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { KeyboardsComponent } from './keyboards/keyboards.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'keyboards', component: KeyboardsComponent },
-  { path: 'about', component: AboutMeComponent }
+  { path: 'about', component: AboutMeComponent },
+  { path: 'blog', component: BlogComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { KeyboardsComponent } from './keyboards/keyboards.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { HomeComponent } from './home/home.component';
 import { SiteFooterComponent } from './site-footer/site-footer.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SiteFooterComponent } from './site-footer/site-footer.component';
     KeyboardsComponent,
     AboutMeComponent,
     HomeComponent,
-    SiteFooterComponent
+    SiteFooterComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
